a4e6bded6af80e6897b6d774a0548118e957b164
Asadullah khan <asadmuhammad427@gmail.com>
2025-01-21 15:22:45 +0500

    inital commit

---
+++ configs/app/features/rollbar.ts
@@ -0,0 +1,42 @@
+import type { Feature } from './types';
+
+import app from '../app';
+import { getEnvValue } from '../utils';
+
+const clientToken = getEnvValue('NEXT_PUBLIC_ROLLBAR_CLIENT_TOKEN');
+const instance = (() => {
+  const envValue = getEnvValue('NEXT_PUBLIC_APP_INSTANCE');
+  if (envValue) {
+    return envValue;
+  }
+
+  return app.host?.replace('.blockscout.com', '').replace('.k8s-dev', '').replaceAll('-', '_');
+})();
+const environment = getEnvValue('NEXT_PUBLIC_APP_ENV') || 'production';
+
+const title = 'Rollbar error monitoring';
+
+const config: Feature<{
+  clientToken: string;
+  environment: string;
+  instance: string | undefined;
+  codeVersion: string | undefined;
+}> = (() => {
+  if (clientToken) {
+    return Object.freeze({
+      title,
+      isEnabled: true,
+      clientToken,
+      environment,
+      instance,
+      codeVersion:"1",
+    });
+  }
+
+  return Object.freeze({
+    title,
+    isEnabled: false,
+  });
+})();
+
+export default config;
---
+++ tools/scripts/dev.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# download assets for the running instance
+dotenv \
+  -e .env.development.local \
+  -e .env.local \
+  -e .env.development \
+  -e .env \
+  -- bash -c './deploy/scripts/download_assets.sh ./public/assets/configs'
+
+source ./deploy/scripts/build_sprite.sh
+echo ""
+
+# generate envs.js file and run the app
+dotenv \
+  -v NEXT_PUBLIC_GIT_COMMIT_SHA=$(git rev-parse --short HEAD) \
+  -e .env.secrets \
+  -e .env.development.local \
+  -e .env.local \
+  -e .env.development \
+  -e .env \
+  -- bash -c './deploy/scripts/make_envs_script.sh && next dev --turbopack -p $NEXT_PUBLIC_APP_PORT' |
+pino-pretty
